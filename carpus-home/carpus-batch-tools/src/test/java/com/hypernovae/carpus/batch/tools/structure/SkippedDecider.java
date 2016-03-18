3855ee79fb3c5067af571727ae79ae9b206fe5a9
alainlompo <alain.lompo@gmail.com>
2016-03-18 02:36:20 +0100

    batch tools initial tests

---
+++ carpus-home/carpus-batch-tools/src/test/java/com/hypernovae/carpus/batch/tools/structure/SkippedDecider.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.hypernovae.carpus.batch.tools.structure;
+
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+public class SkippedDecider implements JobExecutionDecider {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.batch.core.job.flow.JobExecutionDecider#decide(org.springframework.batch.core.JobExecution, org.springframework.batch.core.StepExecution)
+	 */
+	@Override
+	public FlowExecutionStatus decide(JobExecution jobExecution,
+			StepExecution stepExecution) {
+		return stepExecution.getSkipCount() == 0 ? FlowExecutionStatus.COMPLETED : new FlowExecutionStatus("SKIPPED");
+	}
+
+}
